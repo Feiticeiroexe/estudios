9058afa8f2f548ae65570c1933abf1ef4190fc74
Feiticeiroexe <156658369+Feiticeiroexe@users.noreply.github.com>
2024-02-18 21:35:23 +0100

    kuskus

--- Ejercicios_1/Ejercicios_1/Program.cs
+++ Ejercicios_1/Ejercicios_1/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.Data;
 
 namespace Ejercicios_1
 {
@@ -57,22 +58,186 @@ namespace Ejercicios_1
                 Console.ReadLine();
                 //C
                 Console.WriteLine("Parte númerica de las matriculas");
-                for(int i = 0; i < 10; i++)
+                foreach(string matricula in matriculas)
                 {
-                    Console.WriteLine(matriculas[i].Split(("-")[0]));
+                    Console.WriteLine(matricula.Split('-')[0]);
                 }
                 Console.ReadLine();
                 //D
                 Console.WriteLine("Parte alfabética de las matriculas");
-                for (int i = 0; i < 10; i++)
+                foreach(string matricula in matriculas)
                 {
-                    Console.WriteLine(matriculas[i].Split(("-")[1]));
+                    Console.WriteLine(matricula.Split('-')[1]);
                 }
                 Console.ReadLine();
                 //E
                 String ruta = Directory.GetCurrentDirectory() + "\\matriculas.txt";
                 StreamWriter escritor = new StreamWriter(ruta, true);
- 
+                Console.WriteLine(ruta);
+                Console.ReadLine();
+                for(int i = 0; i < 10; i++)
+                {
+                    escritor.WriteLine(matriculas[i]);
+                }
+                escritor.Close();
+            }
+            else if(numEj == 2)
+            {
+                string palabra;
+                char caracter;
+                int contador = 0;
+                Console.WriteLine("Introduzca una frase o palabra:");
+                palabra = Console.ReadLine();
+                Console.WriteLine("Pulse la tecla del caracter que quieras que se cuente cuantas veces aparece en tu frase/palabra:");
+                caracter = Console.ReadKey().KeyChar;
+                Console.WriteLine();
+                for(int i = 0; i < palabra.Length; i++)
+                {
+                    if (palabra[i] == caracter)
+                    {
+                        contador++;
+                    }
+                }
+                Console.WriteLine($"El caracter {caracter} aparece {contador} veces en {palabra}");
+                Console.ReadLine();
+            }
+            else if(numEj == 3)
+            {
+                string DNI;
+                bool corrector = true;
+                do
+                {
+                    corrector = true;
+                    Console.WriteLine("Introduzca su DNI:");
+                    DNI = Console.ReadLine();
+                    if (DNI.Length != 9)
+                    {
+                        corrector = false;
+                    }
+                    else
+                    {
+                        for (int i = 0; i < 8; i++)
+                        {
+                            if (char.IsLetter(DNI[i]))
+                            {
+                                corrector = false;
+                            }
+                        }
+                    }
+                    if (char.IsDigit(DNI[DNI.Length - 1]))
+                    {
+                        corrector = false;
+                    }
+                    Console.Clear();
+                }
+                while (corrector == false);
+                char[] letras = { 'T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E'};
+                string numeros = DNI.Substring(0, 7);
+                int numeros2 = Convert.ToInt32(numeros);
+                int numLetra = (numeros2 % 23) - 1;
+                char letraCorrespondiente = letras[numLetra];
+                char letraOr = char.ToUpper(DNI[DNI.Length - 1]);
+                if(letraCorrespondiente == letraOr)
+                {
+                    Console.WriteLine("El DNI es correcto.");
+                }
+                else
+                {
+                    Console.WriteLine($"La letra esta mal, la correcta seria: {letraCorrespondiente}");
+                }
+                Console.ReadLine();
+            }
+            else if(numEj == 4)
+            {
+                string frase;
+                Console.WriteLine("Introduzca la frase a analizar:");
+                frase = Console.ReadLine();
+                string[] palabras = frase.Split(' ');
+                foreach(string palabra in palabras)
+                {
+                    if(palabra.StartsWith("b") || palabra.StartsWith("B"))
+                    {
+                        Console.WriteLine(palabra);
+                    }
+                }
+                Console.ReadLine();
+            }
+            else if(numEj == 5)
+            {
+                string frase;
+                Console.WriteLine("Introduzca lla frase:");
+                frase = Console.ReadLine();
+                bool yoquese = frase.Contains("@");
+                if(yoquese == true)
+                {
+                    Console.WriteLine("Contriene @");
+                }
+                else
+                {
+                    Console.WriteLine("No contiene @");
+                }
+                Console.ReadLine();
+            }
+            else if(numEj == 6)
+            {
+
+            }
+            else if(numEj == 7)
+            {
+                string correo;
+                Console.WriteLine("Introduce tu correo electronico:");
+                correo = Console.ReadLine();
+                Console.WriteLine(correo.Split('@')[1]);
+                Console.ReadLine();
+            }
+            else if(numEj == 8)
+            {
+                string código;
+                Console.WriteLine("Introduzca el código:");
+                código = Console.ReadLine();
+                if(código.Length == 4)
+                {
+                    if (char.IsLetter(código[0]) && char.IsLetter(código[1]) && char.IsDigit(código[2]) && char.IsDigit(código[3]))
+                    {
+                        Console.WriteLine("El formato es correcto.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Formato incorrecto.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Formato incorrecto.");
+                }
+                Console.ReadLine();
+            }
+            else if(numEj == 9)
+            {
+                string frase;
+                Console.WriteLine("Introduzca la frase:");
+                frase = Console.ReadLine();
+                char[] letras = new char[frase.Length];
+                for(int i = 0; i < frase.Length; i++)
+                {
+                    letras[i] = frase[i];
+                }
+                for (int i = 0; i < letras.Length; i++)
+                {
+                    if (letras[i] == ' ')
+                    {
+                        letras[i] = '*';
+                    }
+                }
+                foreach(char c in letras)
+                {
+                    Console.Write(c);
+                }
+                Console.ReadLine();
+            }
+            else if(numEj == 10)
+            {
+
             }
         }
     }
